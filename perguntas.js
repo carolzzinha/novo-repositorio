454443b1e52c399d3c7b85f261b851757456e0b9
carolini <maria.chodoba@escola.pr.gov.br>
2025-05-26 07:49:17 -0300

    ok

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Moda',
+    'Oque é moda?',
+    'A moda é um conceito amplo que engloba tendências de roupas, acessórios, estilo de vida e comportamento'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos planetas existem no nosso sistema solar?',
+    '8 planetas'
+)
+
+criaCartao(
+    'Programação',
+    'Oque é lógica de programação?',
+    'É a capacidade de pensar de forma estruturada e sistemática para resolver problemas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como eu digo como você está em Inglês?',
+    'Como você esta em ingles é HOW ARE YOU'
+)
